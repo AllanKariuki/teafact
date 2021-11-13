e6215e76d2fec371f086de391746b91926c4fb01
AllanKariuki <77829185+AllanKariuki@users.noreply.github.com>
2021-11-13 01:47:22 -0800

    Deleted directory

--- client/login/login.js
+++
@@ -1,7 +0,0 @@
-const signupBtn = document.querySelectorAll("#signup-btn");
-
-
-signupBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.open("C:\\xampp\\htdocs\\train\\client\\signup\\signup.html");
-});
